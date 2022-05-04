14400a8a096073d448eaab9e7ce89e73cb3329f0
Ev Bogue <ev@evbogue.com>
2022-05-03 19:40:21 -0500

    load a website from a file

--- bin.js
+++ bin.js
@@ -1,4 +1,11 @@
 import { serve } from 'https://deno.land/std@0.137.0/http/server.ts'
-import { post } from './post.js'
 
-serve(() => new Response(post))
+const head = '<html><head><title>Everett Bogue\'s Website on the World Wide Web</title></head><body>'
+
+const foot = '</body></html>'
+
+const body = head + Deno.readTextFileSync('./post.txt') + foot
+
+serve(() => new Response(body, { 
+    headers: { "content-type": "text/html; charset=utf-8"}
+}))
