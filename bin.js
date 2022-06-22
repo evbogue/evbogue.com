bbc30c8a72a7faa6986941b3e7d122e01d77b032
Ev Bogue <ev@evbogue.com>
2022-06-22 17:25:46 -0500

    perhaps this will work

--- bin.js
+++ bin.js
@@ -1,5 +1,7 @@
 import { serve } from 'https://deno.land/std@0.137.0/http/server.ts'
 //import { Marked } from 'https://deno.land/x/markdown@v2.0.0/mod.ts'
+export * as gfm from "https://deno.land/x/gfm@0.1.20/mod.ts";
+
 
 const head = '<html><head><title>Everett Bogue\'s Website on the World Wide Web</title><meta name="viewport" content="width=device-width initial-scale=1" /><meta http-equiv="Content-Type" content="text/html; charset=utf-8"/></head><body>'
 
@@ -8,7 +10,7 @@ const foot = '</body></html>'
 const content = await Deno.readTextFile('./post.md')
 
 function body () { 
-  return head + content + foot
+  return head + gfm.render(content) + foot
 }
 
 serve(() => new Response(body(), { 
