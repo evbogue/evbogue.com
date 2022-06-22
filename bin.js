2571ad332ea7fee7e663dae9939e62db4c77b869
Ev Bogue <ev@evbogue.com>
2022-06-22 17:27:34 -0500

    yah I see what I did wrong

--- bin.js
+++ bin.js
@@ -1,6 +1,6 @@
 import { serve } from 'https://deno.land/std@0.137.0/http/server.ts'
 //import { Marked } from 'https://deno.land/x/markdown@v2.0.0/mod.ts'
-export * as gfm from "https://deno.land/x/gfm@0.1.20/mod.ts";
+import * as gfm from "https://deno.land/x/gfm@0.1.20/mod.ts";
 
 
 const head = '<html><head><title>Everett Bogue\'s Website on the World Wide Web</title><meta name="viewport" content="width=device-width initial-scale=1" /><meta http-equiv="Content-Type" content="text/html; charset=utf-8"/></head><body>'
