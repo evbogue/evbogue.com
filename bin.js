9d2411d2feba25fbac458f8b56a55907594fac07
Ev Bogue <ev@evbogue.com>
2022-06-28 17:27:01 -0500

    update my website

--- bin.js
+++ bin.js
@@ -2,10 +2,11 @@ import { serve } from 'https://deno.land/std@0.137.0/http/server.ts'
 //import { Marked } from 'https://deno.land/x/markdown@v2.0.0/mod.ts'
 import * as gfm from "https://deno.land/x/gfm@0.1.20/mod.ts";
 
+const style = await Deno.readTextFile('./style.css')
 
-const head = '<html><head><title>Everett Bogue\'s Website on the World Wide Web</title><meta name="viewport" content="width=device-width initial-scale=1" /><meta http-equiv="Content-Type" content="text/html; charset=utf-8"/></head><body>'
+const head = '<html><head><title>Everett Bogue\'s Website on the World Wide Web</title><meta name="viewport" content="width=device-width initial-scale=1" /><meta http-equiv="Content-Type" content="text/html; charset=utf-8"/><style>'+ style + '</style></head><body><img src="https://avatars.githubusercontent.com/u/2468866?v=4" style="float:left; width: 175px; height: 175px; margin-right: 1em; margin-bottom: 1em;">'
 
-const foot = '</body></html>'
+const foot = '<iframe src="https://denobook.com/#ELwPcMFe0kRF9luXO7qDSKXsiCOuQR27JT23L3gz3AE=" width="100%;" height="680px;"></iframe></body></html>'
 
 const content = await Deno.readTextFile('./post.md')
 
--- post.md
+++ post.md
@@ -1,14 +1,9 @@
-<img src='https://avatars.githubusercontent.com/u/2468866?v=4' style='width: 175px; height: 175px; float: left; border-radius: 100%; margin-right: 10px;'>
+# Ev Bogue
 
-#### This is my website right now.
+This is the website of Everett Bogue.
 
-<mark>Moving Infrastructure to Deno Deploy</mark> -- some websites are still down.
+I've designed a distributed and secure social network for the modern web.
 
-+ Bogbook - a distributed social networking platform - http://bogbook.com/ [code](http://git.sr.ht/~ev/bogbookv3)
-+ Denobook - a book about Deno - http://denobook.com/ [code](http://git.sr.ht/~ev/denobook/)
+Visit my profile below. Try it in [fullscreen](https://denobook.com/#ELwPcMFe0kRF9luXO7qDSKXsiCOuQR27JT23L3gz3AE=). Read the [code](https://git.sr.ht/~ev/bogbook/).
 
-Please visit my bogbook profile at [http://bogbook.com/#e+2bu3W0KeR00URX75mVtFsFrTgTfAW2R3+F9W+s324=](http://bogbook.com/#e+2bu3W0KeR00URX75mVtFsFrTgTfAW2R3+F9W+s324=)
-
-—_Everett_
-
-[code](http://git.sr.ht/~ev/evbogue.com)
+First, generate a keypair and then you should begin to see my posts. Questions? [ev@evbogue.com](mailto:ev@evbogue.com)
